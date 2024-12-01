f90ae50e491f858b9325c273feb0974606e868a5
YOSHI KATO <chemicalxxxx@gmail.com>
2024-12-01 18:45:46 +1000

    Add deploy.sh

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-set -e # Exit on any error
-
-echo "Starting deployment..."
-
-# Build the site
-bundle exec jekyll build
-
-# Deploy the `_site` directory to the `gh-pages` branch
-ghp-import -n -p -f _site
-
-echo "Deployment complete!"
+echo "Deployment script running..."
